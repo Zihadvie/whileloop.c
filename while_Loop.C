bfd0718fccea78df40cb375736517bd54983a18f
MD. Zihad Islam <159674523+Zihadvie@users.noreply.github.com>
2025-09-25 14:36:09 +0600

    Update while_Loop.C
    use while Loop print 1 to 10 numbers

--- while_Loop.C
+++ while_Loop.C
@@ -1,14 +1,14 @@
 #include<stdio.>
 int main(){
 
-int a=1;
-while(a<=10){
-printf("%d ",a);
-a++;
+int a=1;             //While loop inasialtion
+
+while(a<=10){         //while loop condition
+printf("%d ",a);             //work
+a++;                   //while loop incriment/decriment
 }
 
 
-
-
+return 0;
   
 }
